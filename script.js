dce6152341744500acb740ba969f413b230c93a6
Gaterise <96227732+Gaterise@users.noreply.github.com>
2022-02-04 07:49:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function qrReading() {
 		canvasElement.hidden = false; // canvasを表示する(カメラ映像)
 		canvasElement.height = video.videoHeight; // canvas要素の高さをビデオの高さにする　
 		canvasElement.width = video.videoWidth; // canvas要素の幅をビデオの幅にする
-		canvas.drawImage(video, 0, 0); // カメラ映像をcanvasに表示（引数:image,x座標,y座標）
+		canvas.drawImage(video, 0, 0); // カメラ画像をcanvasに表示（引数:image,x座標,y座標）
 
 		/**
 		 * 指定されたキャンバスの長方形の画像データを含むImageDataオブジェクトを格納
@@ -50,7 +50,7 @@ function qrReading() {
 		); 
 
 		if (code !== null) {// QRコードを認識した場合
-			if (code.data === "") return requestAnimationFrame(qrReading);// QRコードを誤認識してデータが空だった場合繰返し
+			if (code.data === "") window.requestAnimationFrame(qrReading);// QRコードを誤認識してデータが空だった場合繰返し
 			drawLine(code.location.topLeftCorner, code.location.topRightCorner);
 			drawLine(code.location.topRightCorner, code.location.bottomRightCorner);
 			drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner);
@@ -59,7 +59,7 @@ function qrReading() {
 			document.querySelector("#qrdata").value = String(code.data); // input要素にQRコードのデータを表示
 		} 
 	}
-	return requestAnimationFrame(qrReading); // 繰返し
+	window.requestAnimationFrame(qrReading); // 繰返し
 }
 
 /**
@@ -69,13 +69,12 @@ function qrReading() {
  * @param {string} color カラーコード
  */
 function drawLine(begin, end) {
-	canvas.beginPath();
-	console.log(canvas);
-	canvas.moveTo(begin.x, begin.y);
-	canvas.lineTo(end.x, end.y);
+	canvas.beginPath(); // 新しいパスを作成
+	canvas.moveTo(begin.x, begin.y); // x と y で指定した座標にペンを移動
+	canvas.lineTo(end.x, end.y); // 現在の描画位置から x と y で指定した位置に線を描く
 	canvas.lineWidth = 6; // 線の太さ
 	canvas.strokeStyle = "#FF3B58"; // 線の色を指定（"文字列"blue"等でもOK"）
-	canvas.stroke();
+	canvas.stroke(); // 輪郭をなぞる方式で図形を実体化
 }
 
 const loadingMessage = document.getElementById("loadingMessage");
@@ -89,7 +88,7 @@ const canvasElement = document.getElementById("canvas");
 const canvas = canvasElement.getContext("2d");
 
 // カメラ設定
-const constraints = {
+const cameraSettings = {
 	audio: false,
 	video: {
 		width: canvas.width,
@@ -98,24 +97,12 @@ const constraints = {
 	}
   };
 
-// Navigator.mediaDevices 読み取り専用プロパティは、カメラやマイク、画面共有のような接続されたメディア入力装置へのアクセスを提供する MediaDevices オブジェクトを返します。
-// メディアデバイスの準備
-navigator.mediaDevices.getUserMedia(constraints)
-	.then(stream => {
-	// console.log(stream);
-	// console.log(video);
-	video.srcObject = stream;
-	video.setAttribute("playsinline", true); // videoに属性追加（これ無いとiphoneでカメラが正常起動しない）
-	video.play();
+// カメラの起動
+navigator.mediaDevices.getUserMedia(cameraSettings)
+	.then(stream => { // getUserMedia(constraints)でカメラが起動出来た場合、MediaStreamを引数に関数実行
+	video.srcObject = stream; // カメラの映像データ格納
+	video.setAttribute("playsinline", true); // videoに属性追加（無いとiphoneでカメラが正常起動しない）
+	video.play(); // メディアの再生を開始
 	qrReading();
 	}
 );
-
-
-
-
-
-
-
-
-
